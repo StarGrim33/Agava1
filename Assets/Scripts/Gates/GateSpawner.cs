b1b8c29d2c63609a087f6ea205283260f9970ee1
StarGrim33 <prosky33@gmail.com>
2023-05-17 16:20:19 +0300

    оптимизировал систему спавна ворот в зависимости от стороны поля

--- Assets/Scripts/Gates/GateSpawner.cs
+++ Assets/Scripts/Gates/GateSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 public class GateSpawner : MonoBehaviour
 {
     [SerializeField] private Gate _gatePrefab;
-    [SerializeField] private List<Transform> _gatePositions;
+    [SerializeField] private SpawnPointParts _spawnPointParts;
 
     public event UnityAction<Gate> OnGoalGateSpawned;
     public event UnityAction<Vector3> OnGateSpawned;
@@ -34,12 +34,76 @@ public class GateSpawner : MonoBehaviour
     {
         var waitForSeconds = new WaitForSeconds(2f);
         yield return waitForSeconds;
-        int randomPosition = Random.Range(0, _gatePositions.Count);
-        Transform spawnPoint = _gatePositions[randomPosition];
-        _currentGate = Instantiate(_gatePrefab, spawnPoint.position, Quaternion.Euler(-90f, 0f, 0f));
+        var randomPosition = GetRandomPosition();
+        _currentGate = Instantiate(_gatePrefab, randomPosition.position, Quaternion.identity);
+        _currentGate.transform.rotation = DefineRotation(randomPosition);
         _currentGate.OnGoalScored += OnGoalScored;
         OnGoalGateSpawned?.Invoke(_currentGate);
         Vector3 gatePosition = _currentGate.MiddleTarget();
         OnGateSpawned?.Invoke(gatePosition);
+    }
+
+    private Quaternion DefineRotation(Transform spawnPoint)
+    {
+        if (_spawnPointParts.GetLeftSideSpawnPoints().Contains(spawnPoint))
+        {
+            return Quaternion.Euler(-90f, -90f, -90f); 
+        }
+        else if (_spawnPointParts.GetRightSideSpawnPoints().Contains(spawnPoint))
+        {
+            return Quaternion.Euler(-90f, 0f, 0f); 
+        }
+
+        return Quaternion.identity;
+    }
+
+    private Transform GetRandomPosition()
+    {
+        float randomValue = UnityEngine.Random.value;
+
+        if (randomValue < 0.5f)
+        {
+            int randomLeftSidePosition = UnityEngine.Random.Range(0, _spawnPointParts.LeftSideCount);
+            Transform spawnPoint = _spawnPointParts.GetRandomSpawnPoint(randomLeftSidePosition);
+            return spawnPoint;
+        }
+        else
+        {
+            int randomRightSidePosition = UnityEngine.Random.Range(0, _spawnPointParts.RightSideCount);
+            Transform spawnPoint = _spawnPointParts.GetRandomSpawnPoint(randomRightSidePosition);
+            return spawnPoint;
+        }
+    }
+
+    [System.Serializable]
+    public class SpawnPointParts
+    {
+        [SerializeField] private List<Transform> _leftSide;
+        [SerializeField] private List<Transform> _rightSide;
+
+        public int LeftSideCount => _leftSide.Count;
+        public int RightSideCount => _rightSide.Count;
+
+        public Transform GetRandomSpawnPoint(int index)
+        {
+            if (index < _leftSide.Count)
+            {
+                return _leftSide[index];
+            }
+            else
+            {
+                index -= _leftSide.Count;
+                return _rightSide[index];
+            }
+        }
+        public List<Transform> GetLeftSideSpawnPoints()
+        {
+            return _leftSide;
+        }
+
+        public List<Transform> GetRightSideSpawnPoints()
+        {
+            return _rightSide;
+        }
     }
 }
