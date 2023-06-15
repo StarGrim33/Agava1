722e45ea415e6e3c4ffd86b0721427103e9baa03
StarGrim33 <prosky33@gmail.com>
2023-06-15 23:14:58 +0300

    добавление автоматической локализации

--- Assets/Scripts/UI/Yandex SDK/WebUtilityBagFix.cs
+++ Assets/Scripts/UI/Yandex SDK/WebUtilityBagFix.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 
 public class WebUtilityBagFix : MonoBehaviour
 {
+    private const string EnglishCode = "en";
+    private const string RussianCode = "ru";
+    private const string TurkishCode = "tr";
+
     [SerializeField] private LeanLocalization _localization;
+
+    private void Awake()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            ChangeLanguage();
+#endif
+    }
 
     private void OnEnable()
     {
         WebApplication.InBackgroundChangeEvent += OnInBackgroundChange;
-        string language = YandexGamesSdk.Environment.i18n.lang;
-
-        _localization.CurrentLanguage = language switch
-        {
-            "tr" => "Turkey",
-            "ru" => "Russian",
-            "en" => "English",
-            _ => "Russian",
-        };
     }
 
     private void OnDisable()
@@ -30,5 +32,20 @@ public class WebUtilityBagFix : MonoBehaviour
     {
         AudioListener.pause = inBackground;
         AudioListener.volume = inBackground ? 0f : 1f;
+    }
+
+    private void ChangeLanguage()
+    {
+        string languageCode = YandexGamesSdk.Environment.i18n.lang;
+
+        string language = languageCode switch
+        {
+            EnglishCode => "English",
+            RussianCode => "Russian",
+            TurkishCode => "Turkey",
+            _ => "Russian"
+        };
+
+        LeanLocalization.SetCurrentLanguageAll(language);
     }
 }
